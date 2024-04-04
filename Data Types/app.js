ca546d216c2227541239aa73c5fb61cd598630f5
talha12335 <hasanabulhasan131@gmail.com>
2024-04-04 12:11:16 +0500

    Data types completed

--- Data Types/app.js
+++ Data Types/app.js
@@ -54,6 +54,8 @@ var isNotANumber = NaN
 // null is an assignment value, meaning that a variable has been declared and given the value of null .
 // undefined means a variable has been declared but has not yet been assigned a value.
 
+
+
 // 03: Objects
 const person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
 
@@ -75,4 +77,50 @@ var donut = {
 }
 
 console.log(donut.price.large)
-//this is how we can extract the value in nested objects.
+//this is how we can extract the value in nested objects. Object name.key
+
+
+// 04: Arrays
+// Collection of data is called an array 
+
+var myArray = [1, 2, 3, "adhuasjd", "fufjdd", true, "person"]
+// array index starts from 0. 
+// array length is always 1 greater than the indexes.
+// myArray have total 06 indexes.
+// length of myArray is 07.
+
+console.log(myArray[3])
+
+// Arrays have so many methods 
+
+myArray.push("peter")
+// this method is used to add new element at the end of the array. Peter will be added in the last.
+
+myArray.pop()
+// console.log(myArray)
+// .pop() method removes the last element from an array.
+
+myArray.unshift("Parker")
+// the unshift() method adds a new element at the beginning of the array.
+
+myArray.shift()
+// this method removes the first array element and shift all the others to a lower index.
+
+// to concat two arrays we use .concat() method.
+myNamesArray = ["Peter", "Jack", "John", "Jane"]
+concatedArrays = myArray.concat(myNamesArray); // this will concat both arrays.
+console.log(concatedArrays);
+
+var slicedArray = concatedArrays.slice(3, 6)
+console.log(slicedArray, "sliced Array....")
+// .slice() method slices out a piece of an array into a new array.
+// The method then selects elements from the start argument, and up to (but not including) the end argument.
+
+var splicedArray = slicedArray.splice(2, 0, "Zack", "Chris")
+console.log(splicedArray, "spliced Array.....")
+// .splice() method can be used to add or delete items to an array.
+// The first parameter (2) defines the position where new elements should be added (spliced in).
+// The second parameter (0) defines how many elements should be removed.
+// The rest of the parameters ("Zack" , "Chris") define the new elements to be added.
+// The splice() method returns an array with the deleted items.
+
